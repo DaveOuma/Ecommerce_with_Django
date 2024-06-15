abd36354704ee9373198694fbaecdbef6fcf7379
Cynthia <160864573+CynthiaWahome@users.noreply.github.com>
2024-06-16 02:44:12 +0300

    Update models.py
    decreased the indentation for consistency

--- Ecommerce/models.py
+++ Ecommerce/models.py
@@ -12,12 +12,12 @@ class Product(models.Model):
         return self.name
     
 class Customer(models.Model):
-        first_name = models.CharField(max_length=60)
-        last_name = models.CharField(max_length=60)
-        email = models.EmailField(unique=True)
-        phone = models.CharField(max_length=20, blank=True, null=True)
-        address = models.TextField(blank=True, null=True)
-        created_at = models.DateTimeField(auto_now_add=True)
-        
-        def __str__(self):
-            return f"{self.first_name} {self.last_name} {self.email}"
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.email}" 
